590289143147c7d8f7aa250c4a82434ca0f8cf46
nkruiz97 <nkruiz1997@gmail.com>
2026-06-20 05:10:00 -0300

    api actualizada para render.com

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 // Dirección de nuestra API de Django
-const API_URL = "http://127.0.0.1:8000/api/proyectos/";
+const API_URL = "https://mi-portafolio-backend.onrender.com/api/proyectos/";
 
 async function cargarProyectos() {
   const contenedor = document.getElementById("contenedor-proyectos");
